a1fc01ada8307eda92547b1f8b07a9d4b093a2b8
Mainak Mondal <mainakme2140@gmail.com>
2024-08-30 16:43:28 -0400

    Create install_ros2.sh

---
+++ install_ros2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#config ssh
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+service ssh restart
+
+# check locale
+apt update && apt install locales
+locale-gen en_US en_US.UTF-8
+update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+apt install software-properties-common
+add-apt-repository universe
+
+apt update && apt install curl -y
+curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+apt update && apt install ros-dev-tools
+
+apt update
+apt upgrade
+
+apt install ros-iron-desktop
+
+export "source /opt/ros/iron/setup.bash" >> /.bashrc
